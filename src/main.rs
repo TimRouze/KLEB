3dafa778ad3dd3bbbe9762594da21e0ce04a1ad0
TimRouze <timothe.rouze1@gmail.com>
2024-01-31 17:58:05 +0100

    Cleaning + real modimizer

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 #![allow(dead_code)]
 
 mod bloom;
+mod utils;
 use clap::Parser;
 use seq_io::fasta::Reader;
 use std::sync::{Arc, Mutex};
 use std::error::Error;
 use std::fs::File;
-use std::path::Path;
 use std::io::{self, BufRead};
 use std::cmp::min;//bebou
 use std::env;
 use::csv::Writer;
 use::rayon::prelude::*;
 use bloom::{BloomFilter, AggregatingBloomFilter};
-
 
 const K: usize = 31;
 pub type KT = u64;
@@ -39,8 +38,8 @@ struct Args {
     #[arg(short, long, default_value_t = 101010)]
     seed: u64,
     /// Modimizer ?
-    #[arg(short = 'M', long)]
-    modimizer: bool,
+    #[arg(short = 'M', long, default_value_t = 1)]
+    modimizer: u64,
 }
 fn main() {
     let args = Args::parse();
@@ -48,7 +47,7 @@ fn main() {
     let size =  args.memory * 1_000_000_000;
 
     env::set_var("RAYON_NUM_THREADS", args.threads.to_string());
-    if let Ok(lines_temp) = read_lines(input_fof){
+    if let Ok(lines_temp) = utils::read_lines(input_fof){
         let nb_files = lines_temp.count();
         match process_fof_parallel(input_fof, args.modimizer, nb_files, size) {
             Ok(hist_mutex) => {
@@ -64,7 +63,7 @@ fn main() {
 //    var maVariableEstUnChameaubebou
 }
 
-fn process_fof_parallel(filename: &str, modimizer: bool, nb_files: usize, size: usize) -> io::Result<Arc<Mutex<Vec<u64>>>>{
+fn process_fof_parallel(filename: &str, modimizer: u64, nb_files: usize, size: usize) -> io::Result<Arc<Mutex<Vec<u64>>>>{
     let file = File::open(filename)?;
     let reader = io::BufReader::new(file);
     let agregated_BF_mutex_1 = Arc::new(Mutex::new(AggregatingBloomFilter::new_with_seed(size, 1, 333)));//bebou
@@ -84,7 +83,7 @@ fn process_fof_parallel(filename: &str, modimizer: bool, nb_files: usize, size: 
     Ok(hist_mutex)
 }
 
-fn handle_fasta(filename: String, agregated_BF_mutex_1: &Arc<Mutex<AggregatingBloomFilter>>, agregated_BF_mutex_2: &Arc<Mutex<AggregatingBloomFilter>>, bf: &mut BloomFilter, modimizer: bool, hist_mutex: &Arc<Mutex<Vec<u64>>>){
+fn handle_fasta(filename: String, agregated_BF_mutex_1: &Arc<Mutex<AggregatingBloomFilter>>, agregated_BF_mutex_2: &Arc<Mutex<AggregatingBloomFilter>>, bf: &mut BloomFilter, modimizer: u64, hist_mutex: &Arc<Mutex<Vec<u64>>>){
     let mut missing = false;
     let ( reader, _compression) = niffler::get_reader(Box::new(File::open(filename).unwrap())).unwrap();
     let mut fa_reader = Reader::new(reader);
@@ -94,22 +93,15 @@ fn handle_fasta(filename: String, agregated_BF_mutex_1: &Arc<Mutex<AggregatingBl
             let seq = String::from_utf8_lossy(s);
             if seq.len() >= 31{
                 for _i in 0..(seq.len()-K){
-                    let k_mer = str2num(&seq[_i.._i+K]);
-                    if modimizer{
-                        if k_mer%2 == 0{
-                            missing = bf.insert_if_missing(canon(k_mer, rev_comp(k_mer)));
-                        }
-                    }else{
-                        /* println!("kmer: {}\nrevComp: {}\ncanon: {}", num2str(k_mer), num2str(revcomp), num2str(k_mer_canon));
-                        let mut s=String::new();
-                        stdin().read_line(&mut s).expect("Did not enter a correct string"); */
-                        missing = bf.insert_if_missing(canon(k_mer, rev_comp(k_mer)));
+                    let k_mer = utils::str2num(&seq[_i.._i+K]);
+                    if k_mer%modimizer == 0{
+                        missing = bf.insert_if_missing(utils::canon(k_mer, utils::rev_comp(k_mer)));
                     }
                     if missing{
                         let mut agregated_BF_1 = agregated_BF_mutex_1.lock().unwrap();
                         let mut agregated_BF_2 = agregated_BF_mutex_2.lock().unwrap();
-                        let count_1 = agregated_BF_1.add_and_count(canon(k_mer, rev_comp(k_mer)));
-                        let count_2 = agregated_BF_2.add_and_count(canon(k_mer, rev_comp(k_mer)));
+                        let count_1 = agregated_BF_1.add_and_count(utils::canon(k_mer, utils::rev_comp(k_mer)));
+                        let count_2 = agregated_BF_2.add_and_count(utils::canon(k_mer, utils::rev_comp(k_mer)));
                         let mut hist = hist_mutex.lock().unwrap();
                         let min_count = min(count_1, count_2);
                         if min_count < hist.len() as u16{
@@ -136,80 +128,11 @@ fn write_output(hist: Vec<u64>, nb_files: usize) -> Result<(), Box<dyn Error>>{
     Ok(())
 }
 
-
-//bebou
-fn canon(k_mer1: u64, k_mer2:u64) -> u64{
-    min(k_mer1, k_mer2)
-}
-
-fn str2num(k_mer: &str) -> u64{
-    let mut res : u64 = 0;
-    for character in k_mer.chars(){
-        res <<=2;
-        res += (character as u64/2)%4;
-    }
-    res
-}
-
-fn num2str(mut k_mer: u64) -> String{
-    let mut res = String::from("");
-    let mut nuc: u64;
-    for _i in 0..K{
-        nuc = k_mer%4;
-        if nuc == 0{
-            res.push('A');
-        }else if nuc == 1{
-            res.push('C');
-        }else if nuc == 2{//bebou
-            res.push('T');
-        }else if nuc == 3{
-            res.push('G');
-        }
-        k_mer >>= 2;
-    }
-    res.chars().rev().collect()
-}
-
-fn rev_comp(k_mer : u64) -> u64 {
-    let mut res = k_mer.reverse_bits();
-    res = (res >> 1 & 0x5555_5555_5555_5555) | (res & 0x5555_5555_5555_5555) << 1;
-    res ^= 0xAAAA_AAAA_AAAA_AAAA;
-    res >> (2 * (32 - K))
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-#[test]
-fn test_num2str(){
-    let kmer = "AGCTTTTCATTCTGACTGCAACGGGCAATAT";
-    let num = str2num(kmer);
-    assert_eq!(num2str(num), kmer);
-}
-#[test]
-fn test_rc_64() {
-    let kmer = "AGCTTTTCATTCTGACTGCAACGGGCAATAT";
-    let revcomp = rev_comp(str2num(kmer));
-    let res = num2str(revcomp);
-    assert_eq!(res, "ATATTGCCCGTTGCAGTCAGAATGAAAAGCT");
-}
-
-#[test]
-fn test_canon(){
-    let seq = "AGCTTTTCATTCTGACTGCAACGGGCAATAT";
-    let kmer = str2num(seq);
-    let revcomp = rev_comp(kmer);
-    let k_mer_canon = canon(kmer, revcomp);
-    assert_eq!(num2str(k_mer_canon), "ATATTGCCCGTTGCAGTCAGAATGAAAAGCT");
-}
-
 #[test]
 fn test_process_fof_parallel() {
     // Replace "path/to/your/test_file.txt" with the path to your test file
     let filename = "../Data/fof_test.txt";
-    let modimizer = false; // Adjust as needed
+    let modimizer = 1; // Adjust as needed
     let nb_files = 2; // Adjust as needed
     let size = 1_000_000_000; // Adjust as needed
     env::set_var("RAYON_NUM_THREADS", "1");
---
+++ src/utils.rs
@@ -0,0 +1,72 @@
+use std::{cmp::min, fs::File, io::{self, BufRead}, path::Path};
+
+const K: usize = 31;
+pub type KT = u64;
+
+pub fn canon(k_mer1: u64, k_mer2:u64) -> u64{
+    min(k_mer1, k_mer2)
+}
+
+pub fn str2num(k_mer: &str) -> u64{
+    let mut res : u64 = 0;
+    for character in k_mer.chars(){
+        res <<=2;
+        res += (character as u64/2)%4;
+    }
+    res
+}
+
+pub fn num2str(mut k_mer: u64) -> String{
+    let mut res = String::from("");
+    let mut nuc: u64;
+    for _i in 0..K{
+        nuc = k_mer%4;
+        if nuc == 0{
+            res.push('A');
+        }else if nuc == 1{
+            res.push('C');
+        }else if nuc == 2{
+            res.push('T');
+        }else if nuc == 3{
+            res.push('G');
+        }
+        k_mer >>= 2;
+    }
+    res.chars().rev().collect()
+}
+
+pub fn rev_comp(k_mer : u64) -> u64 {
+    let mut res = k_mer.reverse_bits();
+    res = (res >> 1 & 0x5555_5555_5555_5555) | (res & 0x5555_5555_5555_5555) << 1;
+    res ^= 0xAAAA_AAAA_AAAA_AAAA;
+    res >> (2 * (32 - K))
+}
+
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[test]
+fn test_num2str(){
+    let kmer = "AGCTTTTCATTCTGACTGCAACGGGCAATAT";
+    let num = str2num(kmer);
+    assert_eq!(num2str(num), kmer);
+}
+#[test]
+fn test_rc_64() {
+    let kmer = "AGCTTTTCATTCTGACTGCAACGGGCAATAT";
+    let revcomp = rev_comp(str2num(kmer));
+    let res = num2str(revcomp);
+    assert_eq!(res, "ATATTGCCCGTTGCAGTCAGAATGAAAAGCT");
+}
+
+#[test]
+fn test_canon(){
+    let seq = "AGCTTTTCATTCTGACTGCAACGGGCAATAT";
+    let kmer = str2num(seq);
+    let revcomp = rev_comp(kmer);
+    let k_mer_canon = canon(kmer, revcomp);
+    assert_eq!(num2str(k_mer_canon), "ATATTGCCCGTTGCAGTCAGAATGAAAAGCT");
+}
