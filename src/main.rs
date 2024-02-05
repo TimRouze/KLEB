473a6f000eabf51c99ae5b4a1c344741e6d9f936
TimRouze <timothe.rouze1@gmail.com>
2024-02-05 16:36:23 +0100

    safekeeping

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 mod bloom;
 mod utils;
 use clap::Parser;
+use rand::random;
 use seq_io::fasta::Reader;
+use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
 use std::error::Error;
 use std::fs::File;
@@ -40,18 +42,25 @@ struct Args {
     /// Modimizer ?
     #[arg(short = 'M', long, default_value_t = 1)]
     modimizer: u64,
+    /// Number of Agregated Bloom Filter
+    #[arg(short, long, default_value_t = 2)]
+    filters: u64,
 }
+//TODO OPTION NB ABF.
+//TODO: mutex array e.g. 128 mutex avec taille bf divisé en 128 parties 1 mutex par partie
+// Array de BF 
 fn main() {
     let args = Args::parse();
     let input_fof = args.input.as_str();
     let size =  args.memory * 1_000_000_000;
     let hashes = args.hashes;
     let seed = args.seed as u64;
+    let nb_filters = args.filters as u64;
 
     env::set_var("RAYON_NUM_THREADS", args.threads.to_string());
     if let Ok(lines_temp) = utils::read_lines(input_fof){
         let nb_files = lines_temp.count();
-        match process_fof_parallel(input_fof, args.modimizer, nb_files, size, hashes, seed) {
+        match process_fof_parallel(input_fof, args.modimizer, nb_files, size, hashes, seed, nb_filters) {
             Ok(hist_mutex) => {
                 println!("All {} files have been read...\nWriting output...", nb_files);
                 let hist = Arc::try_unwrap(hist_mutex).expect("Failed to unnwrap Arc").into_inner().expect("Failed to get Mutex");
@@ -60,14 +69,14 @@ fn main() {
             Err(err) => eprintln!("Error reading or processing file: {}", err),
         }
     }
-//    var |-ma-variable-est-un-kebab-|
-//    var ma_variable_est_un_serpent
-//    var maVariableEstUnChameaubebou
 }
-
-fn process_fof_parallel(filename: &str, modimizer: u64, nb_files: usize, size: usize, hashes: usize, seed: u64) -> io::Result<Arc<Mutex<Vec<u64>>>>{
+fn process_fof_parallel(filename: &str, modimizer: u64, nb_files: usize, size: usize, hashes: usize, seed: u64, nb_filters: u64) -> io::Result<Arc<Mutex<Vec<u64>>>>{
     let file = File::open(filename)?;
     let reader = io::BufReader::new(file);
+    let mut mutex_vect = Vec::new();
+    for i in 1..nb_filters{
+        mutex_vect.push(Arc::new(Mutex::new(AggregatingBloomFilter::new_with_seed(size, hashes, seed+random::<u64>()))));
+    }
     let agregated_BF_mutex_1 = Arc::new(Mutex::new(AggregatingBloomFilter::new_with_seed(size, hashes, seed+333)));
     let agregated_BF_mutex_2 = Arc::new(Mutex::new(AggregatingBloomFilter::new_with_seed(size, hashes, seed+777)));
     let hist_mutex = Arc::new(Mutex::new(vec![0; nb_files+1]));
@@ -79,14 +88,15 @@ fn process_fof_parallel(filename: &str, modimizer: u64, nb_files: usize, size: u
             let mut bf: BloomFilter = BloomFilter::new_with_seed(size, hashes, seed+1312);
             let filename = line.unwrap();
             println!("{}", filename);
-            handle_fasta(filename, &agregated_BF_mutex_1, &agregated_BF_mutex_2, &mut bf, modimizer, &hist_mutex);
+            handle_fasta(filename,mutex_vect,/* &agregated_BF_mutex_1, &agregated_BF_mutex_2,*/ &mut bf, modimizer, &hist_mutex);
         });
 
     Ok(hist_mutex)
 }
 
-fn handle_fasta(filename: String, agregated_BF_mutex_1: &Arc<Mutex<AggregatingBloomFilter>>, agregated_BF_mutex_2: &Arc<Mutex<AggregatingBloomFilter>>, bf: &mut BloomFilter, modimizer: u64, hist_mutex: &Arc<Mutex<Vec<u64>>>){
+fn handle_fasta(filename: String, mutex_vec: Vec<Arc<Mutex<AggregatingBloomFilter>>>,/*agregated_BF_mutex_1: &Arc<Mutex<AggregatingBloomFilter>>, agregated_BF_mutex_2: &Arc<Mutex<AggregatingBloomFilter>>,*/ bf: &mut BloomFilter, modimizer: u64, hist_mutex: &Arc<Mutex<Vec<u64>>>){
     let mut missing = false;
+    let mut buffer = HashSet::new();
     let ( reader, _compression) = niffler::get_reader(Box::new(File::open(filename).unwrap())).unwrap();
     let mut fa_reader = Reader::new(reader);
     while let Some(record) = fa_reader.next(){
@@ -96,23 +106,33 @@ fn handle_fasta(filename: String, agregated_BF_mutex_1: &Arc<Mutex<AggregatingBl
             if seq.len() >= 31{
                 for _i in 0..(seq.len()-K){
                     let k_mer = utils::str2num(&seq[_i.._i+K]);
+                    let canon = utils::canon(k_mer, utils::rev_comp(k_mer));
                     if k_mer%modimizer == 0{
-                        missing = bf.insert_if_missing(utils::canon(k_mer, utils::rev_comp(k_mer)));
+                        missing = bf.insert_if_missing(canon);
                     }
                     if missing{
+                        buffer.insert(canon);
+                        missing = false;
+                    }
+                    if buffer.len() >= 10000{
+                        /*for mutex in mutex_vec{
+
+                        }*/
                         let mut agregated_BF_1 = agregated_BF_mutex_1.lock().unwrap();
                         let mut agregated_BF_2 = agregated_BF_mutex_2.lock().unwrap();
-                        let count_1 = agregated_BF_1.add_and_count(utils::canon(k_mer, utils::rev_comp(k_mer)));
-                        let count_2 = agregated_BF_2.add_and_count(utils::canon(k_mer, utils::rev_comp(k_mer)));
                         let mut hist = hist_mutex.lock().unwrap();
-                        let min_count = min(count_1, count_2);
-                        if min_count < hist.len() as u16{
-                            hist[min_count as usize] += 1;
-                            if min_count != 1{
-                                hist[(min_count-1) as usize] -= 1;
+                        buffer.iter().for_each(|kmer|{
+                            let count_1 = agregated_BF_1.add_and_count(kmer);
+                            let count_2 = agregated_BF_2.add_and_count(kmer);
+                            let min_count = min(count_1, count_2);
+                            if min_count < hist.len() as u16{
+                                hist[min_count as usize] += 1;
+                                if min_count != 1{
+                                    hist[(min_count-1) as usize] -= 1;
+                                }
                             }
-                        }
-                        missing = false;
+                        });
+                        buffer.clear();
                     }
                 }
             }
